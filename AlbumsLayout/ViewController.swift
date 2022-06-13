33967de4cb666f6210356c0b785dbdaf1e8cd97f
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-06-13 19:34:30 +0300

    Initial Commit

---
+++ AlbumsLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumsLayout
+//
+//  Created by Dmitriy Baskakov on 13.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
